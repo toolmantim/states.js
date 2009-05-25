71b21d96f27574ad72e01cb9689704917e1c5bba
Dmitry Baranovskiy <dmitry.baranovskiy@gmail.com>
2009-05-25 16:47:59 +1000

    Fixing duplicates issue. How I forgot about it?

--- states.js
+++ states.js
@@ -2,18 +2,18 @@
   $(function () {
     var stateNodes = $(".state").add("<i>");
     if (stateNodes.length == 1) return;
-
-    var stateSelector = $("<div id='state-selector'>States: </div>");
+    var stateSelector = $("<div id='state-selector'>States: </div>"),
+        unique = {};
     stateNodes.each(function () {
       var stateName = this.title || "none",
           stateNode = $(this);
-      $("<a href='#'>" + stateName + "</a>").click(function () {
+      unique[stateName] || (unique[stateName] = $("<a href='#'>" + stateName + "</a>").click(function () {
         stateNodes.hide();
         stateNode.show();
         return false;
-      }).appendTo(stateSelector);
+      }).appendTo(stateSelector));
     });
-
+    
     var links = $("a", stateSelector).click(function () {
       links.css("font-weight", 400);
       $(this).css("font-weight", 800);
@@ -31,7 +31,6 @@
       "right": 10,
       "z-index": 99
     });
-
     links.css({
       "margin-left": 10
     });
