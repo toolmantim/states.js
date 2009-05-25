3f0be01c6b8bca21f781f1b3fac9f0947df37027
Dmitry Baranovskiy <dmitry.baranovskiy@gmail.com>
2009-05-25 15:32:29 +1000

    Rewrite it a bit to get rid of code duplication Fixed bug with trailing coma in JSON Cache jQuery calls to variable where possible Something else…

--- states.js
+++ states.js
@@ -1,50 +1,41 @@
-$(function() {
-  if ($(".state").length == 0) return;
-  
-  var stateSelector = $("<div id='state-selector'></div>");
-  stateSelector.append("States: ");
+(function ($) {
+  $(function () {
+    var stateNodes = $(".state").add("<i>");
+    if (stateNodes.length == 1) return;
 
-  var states = [];
-  $(".state").each(function() { states[$(this).attr("title")] = true });
-  
-  for (var state in states) {
-    $("<a href='#'></a>").
-      text(state).
-      click(function() {
-        $(".state").hide();
-        $(".state[title='" + $(this).text() + "']").show();
+    var stateSelector = $("<div id='state-selector'>States: </div>");
+    stateNodes.each(function () {
+      var stateName = this.title || "none",
+          stateNode = $(this);
+      $("<a href='#'>" + stateName + "</a>").click(function () {
+        stateNodes.hide();
+        stateNode.show();
         return false;
-      }).
-      appendTo(stateSelector);
-  }
-  
-  $("<a href='#' class='none'>None</a>").click(function() {
-    $(".state").hide();
-    return false;
-  }).appendTo(stateSelector);
-  
-  stateSelector.find("a").click(function() {
-    stateSelector.find("a").css("font-weight", "normal");
-    $(this).css("font-weight", "bold");
+      }).appendTo(stateSelector);
+    });
+
+    var links = $("a", stateSelector).click(function () {
+      links.css("font-weight", 400);
+      $(this).css("font-weight", 800);
+    });
+    $(links[links.length - 1]).click();
+
+    stateSelector.css({
+      "background-color": "#ddd",
+      "bottom": 10,
+      "color": "#000",
+      "font-size": "14px",
+      "opacity": .7,
+      "padding": "5px 8px",
+      "position": "fixed",
+      "right": 10,
+      "z-index": 99
+    });
+
+    links.css({
+      "margin-left": 10
+    });
+
+    $(document.body).append(stateSelector);
   });
-  
-  stateSelector.find("a.none").click();
-  
-  stateSelector.css({
-    "z-index": "99",
-    "background-color": "#ddd",
-    "color": "#000",
-    "position": "fixed",
-    "bottom": "10px",
-    "right": "10px",
-    "font-size": "14px",
-    "padding": "5px 8px",
-    "opacity": "0.7",
-  });
-  
-  stateSelector.find("a").css({
-    "margin-left": "10px"
-  });
-  
-  $(document.body).append(stateSelector);
-});
+})(jQuery);
